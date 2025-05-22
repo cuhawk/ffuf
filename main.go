ef532bc1c3b00c9c85177dc62f02424796ac02aa
cuhawk <92717259+cuhawk@users.noreply.github.com>
2025-05-22 23:22:45 +0100

    Update main.go

--- main.go
+++ main.go
@@ -116,6 +116,8 @@ func ParseFlags(opts *ffuf.ConfigOptions) *ffuf.ConfigOptions {
 	flag.StringVar(&opts.HTTP.ReplayProxyURL, "replay-proxy", opts.HTTP.ReplayProxyURL, "Replay matched requests using this proxy.")
 	flag.StringVar(&opts.HTTP.RecursionStrategy, "recursion-strategy", opts.HTTP.RecursionStrategy, "Recursion strategy: \"default\" for a redirect based, and \"greedy\" to recurse on all matches")
 	flag.StringVar(&opts.HTTP.URL, "u", opts.HTTP.URL, "Target URL")
+	flag.IntVar(&opts.General.PauseStatus, "pause-status", opts.General.PauseStatus, "HTTP status code to trigger pause")
+	flag.IntVar(&opts.General.PauseDuration, "pause-duration", opts.General.PauseDuration, "Pause duration in minutes when pause-status is encountered")
 	flag.StringVar(&opts.HTTP.SNI, "sni", opts.HTTP.SNI, "Target TLS SNI, does not support FUZZ keyword")
 	flag.StringVar(&opts.Input.Extensions, "e", opts.Input.Extensions, "Comma separated list of extensions. Extends FUZZ keyword.")
 	flag.StringVar(&opts.Input.InputMode, "mode", opts.Input.InputMode, "Multi-wordlist operation mode. Available modes: clusterbomb, pitchfork, sniper")
